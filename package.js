2edbeab237dfc43220d9d235a149cf735646e9f9
Tim Giblin <timmyg13@gmail.com>
2014-11-26 10:22:18 -0500

    load external file

--- package.js
+++ package.js
@@ -1,17 +1,11 @@
 Package.describe({
   name: 'timmyg13:google-prettify',
-  summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  summary: 'Google prettify packaged for meteor',
+  version: '1.0.3',
+  git: 'https://github.com/timmyg/meteor-google-prettify'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('timmyg13:google-prettify.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('timmyg13:google-prettify');
-  api.addFiles('timmyg13:google-prettify-tests.js');
+  api.addFiles('timmyg13:google-prettify.js', 'client');
 });
